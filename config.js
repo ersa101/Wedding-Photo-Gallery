d1dd4caf2befae456e8392dd57526c522f21a286
Shreyas Agrawal <ershreyas101@gmail.com>
2025-12-10 13:34:06 +0000

    Update config.js with correct event data and Google Drive links

--- config.js
+++ config.js
@@ -1,26 +1,62 @@
 const WEDDING_EVENTS = [
   {
-    name: 'Event 1',
-    emoji: '💃',
-    image: 'event1_thumb.jpg',
-    link: 'https://drive.google.com/drive/folders/example1'
+    name: 'THE RUs Shri Ganesh',
+    emoji: '🙏',
+    image: 'ru23dec_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1Abf2RDdFZtlMdmniRCKoTGCDlR1swm-u'
   },
   {
-    name: 'Event 2',
-    emoji: '🌼',
-    image: 'event2_thumb.jpg',
-    link: 'https://drive.google.com/drive/folders/example2'
+    name: 'THE SHREe GANESH<br>with Chaak n Mayera',
+    emoji: '🪔',
+    image: 'shreeganesh_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1H1-fb0mSbOpRQ2B1vi6IpkhfKHjd69eS'
   },
   {
-    name: 'Event 3',
+    name: 'THE MEHENDI<br>RuSh का रंग ',
+    emoji: '🌿',
+    image: 'mehendi_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1ipgxlnPTosNetzr-eDEo5pIYkdG6OkGo'
+  },
+  {
+    name: 'THE SAGAI<br>दिलों की सौदेबाज़ी',
     emoji: '💍',
-    image: 'event3_thumb.jpg',
-    link: 'https://drive.google.com/drive/folders/example3'
+    image: 'sagai_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1GyNdPb2leiYY-Jmo-hcBzOrqtE_Rr6os'
   },
   {
-    name: 'Event 4',
+    name: 'THE SANGEET<br>नाचेंगे सारी रात',
+    emoji: '💃',
+    image: 'sangeet_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1RiTaOieOdaQgi_djAkdYh5V5V8MqgLZX'
+  },
+  {
+    name: 'THE HALDI<br>पीली धूप पहन के तुम',
+    emoji: '🌼',
+    image: 'haldi_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1lEZKOe9ACcvxo2PhiY7--yneDA8Z7f40'
+  },
+  {
+    name: 'THE GAURI PUJA<br>ॐ देवी महागौर्यै नमः',
+    emoji: '🌸',
+    image: 'gauripuja_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1GN6j4kGLHPUOYTYiheJkfu4DwaS1db6x'
+  },
+  {
+    name: 'THE BAARAAT<br>दुल्हन हम ले जाएंगे',
+    emoji: '🐎',
+    image: 'baaraat_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1satuU9386OHIGRhQkCWEsWmmioXGuSne'
+  },
+  {
+    name: 'THE VOWS<br>वादा 7 जन्मों का ',
+    emoji: '🔥',
+    image: 'fere_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/1LJGb96n_61I-dtR3VcSbOLY25VX0l8LX'
+  },
+  {
+    name: 'THE OTHER RECEPTION<br> at Bhopal',
     emoji: '🥂',
-    image: 'event4_thumb.jpg',
-    link: 'https://drive.google.com/drive/folders/example4'
+    image: 'bplreception_thumb_v2.jpg',
+    link: 'https://drive.google.com/drive/folders/15mnPzEgtaLldss-M7A4JpdWLfE3EvXti'
   }
 ];
