c41ad5df87e7a81d30f9b270a0c89aeab8868386
Shreyas Agrawal <ershreyas101@gmail.com>
2025-12-10 13:24:01 +0000

    Fix: Improve petals visibility, gallery layout, background image, and remove invalid CSS

---
+++ config.js
@@ -0,0 +1,26 @@
+const WEDDING_EVENTS = [
+  {
+    name: 'Event 1',
+    emoji: '💃',
+    image: 'event1_thumb.jpg',
+    link: 'https://drive.google.com/drive/folders/example1'
+  },
+  {
+    name: 'Event 2',
+    emoji: '🌼',
+    image: 'event2_thumb.jpg',
+    link: 'https://drive.google.com/drive/folders/example2'
+  },
+  {
+    name: 'Event 3',
+    emoji: '💍',
+    image: 'event3_thumb.jpg',
+    link: 'https://drive.google.com/drive/folders/example3'
+  },
+  {
+    name: 'Event 4',
+    emoji: '🥂',
+    image: 'event4_thumb.jpg',
+    link: 'https://drive.google.com/drive/folders/example4'
+  }
+];
